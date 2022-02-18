5d4d43c791b9d79b4df3930bf32b07cc0a7f0ca6
Seheon Yu <seyu@student.42seoul.kr>
2022-02-18 15:52:42 +0900

    refactor: Improve readability of exercise 1-03
    Define square function

--- 1-03.js
+++ 1-03.js
@@ -1,9 +1,13 @@
+function square(x) {
+	return x * x;
+}
+
 function sum(n1, n2, n3) {
 	return (
 		n1 < n2 && n1 < n3 ?
-			n2 * n2 + n3 * n3 :
+			square(n2) + square(n3) :
 			n2 < n3 && n2 < n1 ?
-				n1 * n1 + n3 * n3 :
-				n1 * n1 + n2 * n2
+				square(n1) + square(n3) :
+				square(n1) + square(n2)
 	);
 }
