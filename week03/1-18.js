51da4c10bc70857dab870cb29500fed14d7f71b9
Seheon Yu <seyu@student.42seoul.kr>
2022-03-06 16:45:38 +0900

    feat: Remove parameter "sum" from "fast_times"
    SRP, ISP (SOLID)
    
    close #1

--- week03/1-18.js
+++ week03/1-18.js
@@ -10,7 +10,7 @@ function is_even(x) {
 	return x % 2 === 0;
 }
 
-function fast_times(sum, a, b) {
+function fast_times_iter(sum, a, b) {
 	return (
 		b === 0
 		? 0
@@ -20,4 +20,8 @@ function fast_times(sum, a, b) {
 		? fast_times(sum, double(a), halve(b)) // grows logarithmically with b in number of steps
 		: fast_times(sum + a, a, b - 1) // generate iterative process
 	);
+}
+
+function fast_times(a, b) {
+	return fast_times_iter(0, a, b);
 }
